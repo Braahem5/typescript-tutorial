ea2fddc481a0f9a1a430d2ab1e148e2a87076fcf
IBRAHIM <ibrahimbajepade555@gmail.comgit config --global user.name IBRAHIM>
2023-11-18 22:57:40 +0300

    learning of classes and type guard

--- dist/index.js
+++ dist/index.js
@@ -32,6 +32,8 @@ let employeeUsingAlias = {
         console.log("retired on " + date);
     },
 };
+const centerPoint = { x: 0, y: 0 };
+const myCircle = { center: centerPoint, radius: 4 };
 function KgToLbs(weight) {
     if (typeof weight === "number") {
         let variable = weight * 2.2;
@@ -56,6 +58,19 @@ function greet(name) {
         console.log("Hola!");
 }
 greet(null);
+const john = {
+    name: "john",
+    age: 25,
+    address: "123 Main St, New York",
+};
+const JonhNameAndAge = {
+    name: "John",
+    age: 36,
+};
+const JohnWithoutAge = {
+    name: "John",
+    address: "123 Main St New York",
+};
 function getCustomer(id) {
     return id === 0 ? null : { birthday: new Date() };
 }
@@ -69,4 +84,90 @@ let log = (year) => {
     return date;
 };
 console.log(log === null || log === void 0 ? void 0 : log(2002));
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    makeSound() {
+        console.log("I am an animal");
+    }
+}
+const myAnimal = new Animal("Parrot", 3);
+console.log(`Name: ${myAnimal.name}, Age: ${myAnimal.age}`);
+myAnimal.makeSound();
+function greetPerson(person) {
+    if (typeof person === "object") {
+        const p = person;
+        console.log(`Hello ${p.firstName}!`);
+    }
+    else {
+        throw Error("Invalid input");
+    }
+}
+const Ibrahim = { firstName: "Ibrahim", lastName: "Bajepade", age: 21 };
+greetPerson(Ibrahim);
+class Culture {
+    constructor(firstName, lastName, age, national) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.national = national;
+    }
+    greet() {
+        return (`Hello my name is ${this.firstName + " " + this.lastName} and i am from ${this.national.country}. my main language is ${this.national.language}` +
+            (this.national.otherLanguage
+                ? `and i also speak ${this.national.otherLanguage}`
+                : ""));
+    }
+}
+function addressPeople(welcomeWord) {
+    if (!welcomeWord.firstName && !welcomeWord.lastName) {
+        throw new Error("First name and Last Name are required");
+    }
+    if (welcomeWord instanceof Culture) {
+        switch (welcomeWord.national.nationality) {
+            case "Russian":
+                console.log(welcomeWord.greet());
+                console.log(`Приветствую вас ${welcomeWord.firstName} ${welcomeWord.lastName}.`);
+                break;
+            case "Chinese":
+                console.log(welcomeWord.greet());
+                console.log(`你好，${welcomeWord.firstName} ${welcomeWord.lastName}.`);
+                break;
+            default:
+                console.log(welcomeWord.greet());
+                console.log(`Hello, ${welcomeWord.firstName} ${welcomeWord.lastName}.`);
+        }
+    }
+}
+const samplePerson = {
+    nationality: "Chinese",
+    language: "Mandarin",
+    country: "china",
+    otherLanguage: "English",
+};
+const Audience = new Culture("Max", "Kushut", 25, samplePerson);
+addressPeople(Audience);
+function getArea(shape) {
+    if (isCircle(shape)) {
+        return Math.round(Math.PI * Math.pow(shape.radius, 2));
+    }
+    else if (isRectangle(shape)) {
+        return shape.width * shape.length;
+    }
+    else {
+        const exhaustiveCheck = shape;
+        return exhaustiveCheck;
+    }
+}
+function isCircle(shape) {
+    return shape.kind === "circle";
+}
+function isRectangle(shape) {
+    return shape.kind === "rectangle";
+}
+const cir = { kind: "circle", radius: 5 };
+const rec = { kind: "rectangle", width: 4, length: 6 };
+console.log(`The shape is ${rec.kind} and area is ${getArea(rec)}`);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,13 @@ let employeeUsingAlias: Employee = {
   },
 };
 
+//*************** complex use of Alias */
+type Point = { x: number; y: number };
+type Circle = { center: Point; radius: number };
+
+const centerPoint: Point = { x: 0, y: 0 };
+const myCircle: Circle = { center: centerPoint, radius: 4 };
+
 // Union Type
 function KgToLbs(weight: number | string): number {
   if (typeof weight === "number") {
@@ -137,7 +144,7 @@ type PersonNameAddress = Omit<Person, "age">;
 const JohnWithoutAge: PersonNameAddress = {
   name: "John",
   address: "123 Main St New York",
-}
+};
 
 //Optional Chaining
 type Customer = {
@@ -163,3 +170,155 @@ let log: any = (year: number): Date => {
   return date;
 };
 console.log(log?.(2002)); //Optional call
+
+//************* Classes */
+class Animal {
+  //properties
+  name: string;
+  age: number;
+
+  //constructor
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  makeSound() {
+    console.log("I am an animal");
+  }
+}
+
+// Create an instance of the class
+const myAnimal = new Animal("Parrot", 3);
+
+//Access properties
+console.log(`Name: ${myAnimal.name}, Age: ${myAnimal.age}`);
+
+//Call methods
+myAnimal.makeSound();
+
+///****************** Type guarding  ******************/
+//Example 1 (using the typeof operator)
+interface Human {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
+function greetPerson(person: Human) {
+  if (typeof person === "object") {
+    const p = person as Human;
+    console.log(`Hello ${p.firstName}!`);
+  } else {
+    throw Error("Invalid input");
+  }
+}
+const Ibrahim: Human = { firstName: "Ibrahim", lastName: "Bajepade", age: 21 };
+greetPerson(Ibrahim);
+
+//Example 2 (Instanceof Type Guards)
+interface nationalInfo {
+  nationality: string;
+  language: string;
+  country: string;
+  otherLanguage?: string;
+}
+class Culture implements Human {
+  firstName: string;
+  lastName: string;
+  age?: number;
+  national: nationalInfo;
+  constructor(
+    firstName: string,
+    lastName: string,
+    age: number,
+    national: nationalInfo
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.national = national;
+  }
+  greet() {
+    return (
+      `Hello my name is ${this.firstName + " " + this.lastName} and i am from ${
+        this.national.country
+      }. my main language is ${this.national.language}` +
+      (this.national.otherLanguage
+        ? `and i also speak ${this.national.otherLanguage}`
+        : "")
+    );
+  }
+}
+
+function addressPeople(welcomeWord: any): void {
+  if (!welcomeWord.firstName && !welcomeWord.lastName) {
+    throw new Error("First name and Last Name are required");
+  }
+  if (welcomeWord instanceof Culture) {
+    switch (welcomeWord.national.nationality) {
+      case "Russian":
+        console.log(welcomeWord.greet());
+        console.log(
+          `Приветствую вас ${welcomeWord.firstName} ${welcomeWord.lastName}.`
+        );
+        break;
+      case "Chinese":
+        console.log(welcomeWord.greet());
+        console.log(`你好，${welcomeWord.firstName} ${welcomeWord.lastName}.`);
+        break;
+      default:
+        console.log(welcomeWord.greet());
+        console.log(`Hello, ${welcomeWord.firstName} ${welcomeWord.lastName}.`);
+    }
+  }
+}
+
+const samplePerson: nationalInfo = {
+  nationality: "Chinese",
+  language: "Mandarin",
+  country: "china",
+  otherLanguage: "English",
+};
+const Audience = new Culture("Max", "Kushut", 25, samplePerson);
+addressPeople(Audience);
+
+//Example 3 (User-Defined Type Guards).
+interface Circle1 {
+  kind: "circle";
+  radius: number;
+}
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  length: number;
+}
+
+// Define the Shape type as a union of Circle and Rectangle
+type Shape = Circle1 | Rectangle;
+
+//function to calculate the area of the circle and rectangle.
+function getArea(shape: Shape): number {
+  if (isCircle(shape)) {
+    // Use the formula for the area of a circle
+    return Math.round(Math.PI * Math.pow(shape.radius, 2));
+  } else if (isRectangle(shape)) {
+    // Use the formula for the area of a rectangle
+    return shape.width * shape.length;
+  } else {
+    // TypeScript knows that this case is exhaustive
+    const exhaustiveCheck: never = shape;
+    return exhaustiveCheck;
+  }
+}
+
+// Implement type guards for Circle and Rectangle
+function isCircle(shape: Shape): shape is Circle1 {
+  return shape.kind === "circle";
+}
+function isRectangle(shape: Shape): shape is Rectangle {
+  return shape.kind === "rectangle";
+}
+
+// Test the calculateArea function with different shapes
+const cir: Shape = { kind: "circle", radius: 5 };
+const rec: Shape = { kind: "rectangle", width: 4, length: 6 };
+console.log(`The shape is ${rec.kind} and area is ${getArea(rec)}`);
